03b5d10b6d7b1c8f5dbafa68ddab161ef08ddd77
sander_tcr <9024411@student.tcrmbo.nl>
2024-09-17 16:02:23 +0200

    opdracht 2

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-gsap.from(".fred", { opacity: 0, y: 100, duration: 1 });
+gsap.to(".fred", { x: 800, backgroundColor: 'red', scale: 1.2, duration: 1 }, "ease out");
+
+gsap.from(".fre", { x: 200, y: 100, duration: 1 });
+
+gsap.to(".fred3", { x: 500, y: 500, duration: 1 });
