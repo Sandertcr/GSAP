c6b2335fe8b4033ec63d7c8cdc4c3808286297b8
sander_tcr <9024411@student.tcrmbo.nl>
2024-09-17 16:12:26 +0200

    opdracht 2

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 gsap.to(".fred", { x: 800, backgroundColor: 'red', scale: 1.2, duration: 1 }, "ease out");
 
-gsap.from(".fre", { x: 200, y: 100, duration: 1 });
+gsap.from(".fred2", { x: 200, y: 100, duration: 1 });
 
 gsap.to(".fred3", { x: 500, y: 500, duration: 1 });
+
+let text = document.querySelector(".fred2");
+
+let animation = gsap.to(".fred2", {
+  paused: true,
+  scale: 1.2
+});
+
+text.addEventListener("mouseenter", () => animation.play());
+text.addEventListener("mouseleave", () => animation.reverse());
